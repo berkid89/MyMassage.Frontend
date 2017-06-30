2a0f884b8393e2e116a5b6c2c90047c08f9f3296
berkid89 <berkid89@gmail.com>
2017-06-30 15:36:49 +0200

    GET IN TOUCH

--- landing/src/app/app.component.ts
+++ landing/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { SpecialOffer } from './models/special-offer';
 import { Http } from '@angular/http';
+import { GetInTouch } from './models/get-in-touch';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,14 @@ export class AppComponent {
   public settings: any;
   public year: number = new Date().getFullYear();
   public emailAddressForSubscribe: string;
-  public specialOffer: SpecialOffer
+  public specialOffer: SpecialOffer;
+  public getInTouch: GetInTouch;
 
   constructor(private translate: TranslateService, private http: Http) {
     this.setLang('hu');
     this.initSettings();
     this.initSpecialOffer();
+    this.initGetInTouch();
   }
 
   setLang(lang: string) {
@@ -41,5 +44,16 @@ export class AppComponent {
   initSettings() {
     this.http.get('/assets/settings.json')
       .subscribe(res => this.settings = res.json());
+  }
+
+  initGetInTouch() {
+    let git = new GetInTouch();
+    git.address = "Károly krt. 3/a. Fél emelet 13. (33-as kapucsengő)";
+    git.city = "Budapest 1075";
+    git.country = "Magyarország";
+    git.phoneNumber = "+36/30-916-1147";
+    git.email = "masszazsterapia.eletmod@gmail.com";
+
+    this.getInTouch = git;
   }
 }
---
+++ landing/src/app/models/get-in-touch.ts
@@ -0,0 +1,7 @@
+export class GetInTouch {
+    public address: string;
+    public city: string;
+    public country: string;
+    public phoneNumber: string;
+    public email: string;
+}
